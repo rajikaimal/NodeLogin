038dda3f07feaa40f733a8b6e905489c8c11653c
Rajika Imal <inbox.rajika@gmail.com>
2015-05-18 22:15:10 +0530

    mysql support

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
-var mongojs = require('mongojs');
-var db = mongojs('logininfo',['logininfo']); //mongodb and collection name - logininfo
+var mysql = require('mysql');
 var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : 'miyoungrae123',
+	database : 'node-login'
+});
+connection.connect();
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+
 app.get('/home',function(req,res){
 	console.log('I recieved a get request');
 	res.send("Welcome User ");
@@ -15,17 +24,22 @@ app.post('/login',function(req,res){
 	console.log(req.body.password);
 	var Username = req.body.username;
 	var Password = req.body.password;
-	db.logininfo.find({ username : Username , password : Password},function(err,data){
-		if(err){
-			console.log("Invalid combination");
-			res.send("Invalid combination");
+	connection.query('SELECT * FROM users where username = ' + connection.escape(Username) +' AND password = ' + connection.escape(Password),function(err,rows,fields){
+		if(!err){
+			if(Username == rows[0]['username'] && Password == rows[0]['password']){
+				console.log('Logged in ');
+			}
+			else{
+				console.log('Invalid combination');
+			}
 		}
 		else{
-			console.log(data);
-			res.redirect(302,'http://localhost:3000/home');
+			console.log('Oops something went wrong');
 		}
 	});
+
 });
 
-app.listen(3000);
-console.log('Server running on port 3000');
+app.listen(3000,function(){
+	console.log('Server running at port 3000');
+});
